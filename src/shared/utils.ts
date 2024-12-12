1c9f29f97d4a2bcc147433ed58c5b30060a12ad7
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-12-11 22:21:19 -0300

    Revert "Initial commit"
    This reverts commit 80e2331aaf4c1d7ae7e69ffe04fec040de033434.

--- src/shared/utils.ts
+++
@@ -1,7 +0,0 @@
-export async function safeExecute(fn: Function, ...args: any[]) {
-  try {
-    await fn(...args);
-  } catch ( error ) {
-    console.error(`Error executing ${ fn.name }:`, error);
-  }
-}
